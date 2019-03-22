477181ba98b048eef3e5c0607439cbb938bc556a
Edson Lucas Carlotti <ecarlotti@dpvmdev.leadcomm.local>
2019-03-22 12:58:21 -0300

    removed unnecessary version restrictions

--- package.js
+++ package.js
@@ -1,15 +1,17 @@
 Package.describe({
   name: 'urbanetic:utility',
   summary: 'A collection of utility modules',
-  version: '1.2.3',
+  version: '1.2.3_1',
   git: 'https://github.com/urbanetic/meteor-utility.git'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   Npm.depends({
     '@urbanetic/utility': '0.1.6'
   });
+
   api.versionsFrom('METEOR@1.2.1');
+
   api.use([
     'coffeescript',
     'underscore',
@@ -17,6 +19,7 @@ Package.onUse(function(api) {
     'aramk:q@1.0.1_1',
     'aramk:tinycolor@1.1.0_1',
   ], ['client', 'server']);
+
   api.use([
     'deps',
     'jquery',
@@ -25,30 +28,33 @@ Package.onUse(function(api) {
   ], 'client');
 
   // Compile the Node.js package for server and client, and export the global Services namespace.
-  api.use(['cosmos:browserify@0.5.0']);
+  api.use(['cosmos:browserify']);
   api.addFiles(['src/lib.browserify.js']);
 
   api.use([
-    'aldeed:autoform@5.8.1',
-    'momentjs:moment@2.10.3',
-    'matb33:collection-hooks@0.8.0',
-    'semantic:ui-css@2.0.8',
-    'session@1.1.0',
-    'reactive-var@1.0.5',
-    'tracker@1.0.5'
-  ], 'client', {weak: true});
+    'aldeed:autoform',
+    'momentjs:moment',
+    'matb33:collection-hooks',
+    'semantic:ui-css',
+    'session',
+    'reactive-var',
+    'tracker'
+  ], 'client', { weak: true });
+
   // Either of these will contain the Async module, but we don't want to strongly require either
   // since we don't know which is being used.
   api.use([
-    'meteorhacks:async@1.0.0',
-    'meteorhacks:npm@1.2.2'
-  ], 'server', {weak: true});
+    'meteorhacks:async',
+    'meteorhacks:npm'
+  ], 'server', { weak: true });
+
   api.use([
-    'aldeed:simple-schema@1.5.3',
-    'aldeed:collection2@2.3.3'
+    'aldeed:simple-schema',
+    'aldeed:collection2'
   ], ['client', 'server']);
+
   // Make these available to the app to allow working with tiem and deferreds.
-  api.imply(['momentjs:moment', 'aramk:q'], ['client','server'])
+  api.imply(['momentjs:moment', 'aramk:q'], ['client', 'server']);
   api.export([
     'Collections',
     'Colors',
@@ -60,14 +66,17 @@ Package.onUse(function(api) {
     'Environment',
     'Promises'
   ], ['client', 'server']);
+
   api.export([
     'Buffers'
   ], 'server');
+
   api.export([
     'Blobs',
     'Forms',
     'Templates'
   ], 'client');
+
   api.addFiles([
     'src/Collections.coffee',
     'src/Colors.coffee',
@@ -79,13 +88,16 @@ Package.onUse(function(api) {
     'src/Environment.coffee',
     'src/Promises.coffee'
   ], ['client', 'server']);
+
   api.addFiles([
     'src/Blobs.coffee',
     'src/Forms.coffee',
     'src/Templates.coffee',
     'src/forms.less'
   ], 'client');
+
   api.addFiles([
     'src/Buffers.coffee'
   ], 'server');
+  
 });
